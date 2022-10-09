66bad3acb48c1928d40fb3eac5e0f3f95207d358
Senicad <senicad@go2infinity.com>
2022-10-09 14:30:02 -0400

    File System Update

---
+++ htdocs/js/main.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    // data: function(){
+    //     return {
+    //         product: "Socks"
+    //     }
+    // }
+    
+    // ** NOTE **
+    // Above is equivalent to below
+
+    data(){
+        return {
+            product: "Socks"
+        }
+    }
+})
