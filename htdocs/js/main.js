e129a87237f2707be39a6eb5dcc457525d7b44b9
Senicad <senicad@go2infinity.com>
2022-10-09 15:11:37 -0400

    List Rendering and prior (Section 5)

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -10,7 +10,15 @@ const app = Vue.createApp({
 
     data(){
         return {
-            product: "Socks"
+            product  : "Socks", // Key -> Value
+            image    : './assets/images/socks_green.jpg', // Path from HTML File
+            // inStock : true
+            inventory: 10,
+            details  : ['50% corron', '30% wool', '20% polyester'],
+            variants : [
+                {id: 2234, color: 'green'},
+                {id: 2235, color: 'blue'},
+            ] 
         }
     }
 })
