bfeb0e7ca9882d2e63989a613c9883a70a13c0f7
Senicad <senicad@go2infinity.com>
2022-10-09 15:26:09 -0400

    Lesson 6 (Event Handeling)

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,24 +1,24 @@
 const app = Vue.createApp({
-    // data: function(){
-    //     return {
-    //         product: "Socks"
-    //     }
-    // }
-    
-    // ** NOTE **
-    // Above is equivalent to below
-
-    data(){
+    data() {
         return {
-            product  : "Socks", // Key -> Value
-            image    : './assets/images/socks_green.jpg', // Path from HTML File
-            // inStock : true
-            inventory: 10,
-            details  : ['50% corron', '30% wool', '20% polyester'],
-            variants : [
-                {id: 2234, color: 'green'},
-                {id: 2235, color: 'blue'},
-            ] 
+            cart:0,
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+            ]
+        }
+
+    },
+    methods:{
+        addToCart(){
+            this.cart+=1
+        },
+        updaetImage(variantImage){
+            this.image = variantImage;
         }
     }
 })
